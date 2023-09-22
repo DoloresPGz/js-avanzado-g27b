2aad6e2842041de36e96e4a5f18a9d2ea12c5300
DoloresPGz <dolores.pgz@gmail.com>
2023-09-21 19:25:59 -0600

    ejemplo promise all

---
+++ asincronismo/promiseall.js
@@ -0,0 +1,26 @@
+function prepararPizza(especialidad,tiempo){
+    return new Promise((res, rej) =>{
+        setTimeout(() => {
+            if(tiempo < 3000)
+                res(`Pizza preparada:${especialidad}`)
+            else
+                rej('La pizza quedó fría')
+        }, tiempo);
+    })
+}
+
+const ordenPizzas = [
+    prepararPizza('Hawaiana', 2000),
+    prepararPizza('3 Quesos', 3001),
+    prepararPizza('Pastor', 2500),
+    prepararPizza('Mexicana', 1500),
+]
+
+
+Promise.all(ordenPizzas).then(
+    pizzas => {
+        pizzas.forEach( pizza => console.log(pizza))
+    }
+).catch(err => console.log(err))
+
+    
