c8f151ed7ce37ddcac4cbcc78244787a68206d86
DoloresPGz <dolores.pgz@gmail.com>
2023-10-03 20:03:38 -0600

    loadUsers en proceso

---
+++ ejemplos-api/gestion-usuarios/script.js
@@ -0,0 +1,19 @@
+const nameInput = document.getElementById("name")
+const emailInput = document.getElementById("email")
+const userList = document.getElementById("user-list")
+const crudForm = document.getElementById("crud-form")
+const apiKey = "" //AQUÍ VA TU API KEY
+const apiURL = `https://crudcrud.com/api/${apiKey}/users`
+
+function loadUsers() {
+    fetch(apiURL)
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data)
+    })
+}
+
+
+document.activeElement("DOMContentLoaded", function(){
+    loadUsers()
+})
